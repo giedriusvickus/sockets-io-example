de8ad15060950786d538a28890195558324ca193
Giedrius <giedrius.vickus@gmail.com>
2021-02-04 09:33:12 +0200

    SocketIo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Feed from "./Feed";
 
 function App() {
-    const [messageHistory, setMessageHistory] = useState([]);
-    const [lastMessage, setLastMessage] = useState([]);
-
-    useEffect(() => {
-        lastMessage && setMessageHistory((prev) => prev.concat(lastMessage));
-    }, [lastMessage]);
-
     return (
         <div className="App">
             <header className="App-header">
-                <Feed setMessages={(data) => setLastMessage(data)} />
-                <div>
-                    {messageHistory ? (
-                        <ul>
-                            {messageHistory.map((message, idx) => (
-                                <li key={idx}>{message}</li>
-                            ))}
-                        </ul>
-                    ) : null}
-                </div>
+                <Feed />
             </header>
         </div>
     );
--- src/Feed.js
+++ src/Feed.js
@@ -1,16 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://localhost:4001";
 
-const Feed = ({ setMessages }) => {
+const Feed = () => {
+    const [messageHistory, setMessageHistory] = useState([]);
+    const [lastMessage, setLastMessage] = useState(null);
+
     useEffect(() => {
         const socket = socketIOClient(ENDPOINT);
         socket.on("newData", (data) => {
-            setMessages(data);
+            setLastMessage(data);
         });
     }, []);
 
-    return <span />;
+    useEffect(() => {
+        lastMessage && setMessageHistory((prev) => prev.concat(lastMessage));
+    }, [lastMessage]);
+
+    return (
+        <div>
+            {messageHistory ? (
+                <ul>
+                    {messageHistory.map((message, idx) => (
+                        <li key={idx}>{message}</li>
+                    ))}
+                </ul>
+            ) : null}
+        </div>
+    );
 };
 
 export default Feed;
